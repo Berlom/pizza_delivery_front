a5011aeab9baa8ead5ea399e6a6fbdaa8cba2cde
Berlom <mizou7amza@live.fr>
2022-01-22 02:30:34 +0100

    here lies my hopes to finish this project

--- pages/cart/[id].jsx
+++ pages/cart/[id].jsx
@@ -5,22 +5,31 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen } from "@fortawesome/free-solid-svg-icons";
+import { faMinus, faPen, faPlus } from "@fortawesome/free-solid-svg-icons";
 export default function cartItem() {
   const router = useRouter();
   const [cart, setCart] = useState(null);
   const [menu, setMenu] = useState([]);
+  const [quantity, setQuantity] = useState(0);
+  const [ingredients, setIngredients] = useState([]);
+  const [ings, setIngs] = useState([]);
 
   const [editable, setEditable] = useState(false);
 
   const update = async () => {
     try {
+      let ingString = "";
+      ingredients.map((elt) => {
+        if (ings.includes(elt.name)) ingString += elt.id + "@";
+      });
+      ingString = ingString.substring(0, ingString.length - 1);
+      console.log(ingString);
       const response = await axios.put(
         "http://localhost:8000/api/cart/update/" + router.query.id,
         {
           menu_id: cart.cart.menu_id,
           quantity: cart.cart.quantity,
-          ingredient: cart.cart.ingredients,
+          ingredient: ingString,
         },
         {
           headers: {
@@ -31,6 +40,7 @@ export default function cartItem() {
         }
       );
       console.log(response.data);
+      window.location.reload(true);
     } catch (error) {
       console.log(error);
     }
@@ -58,10 +68,31 @@ export default function cartItem() {
       });
       setMenu(menus.data);
       setCart(panier.data);
+      setQuantity(panier.data.cart.quantity);
+      setIngs(panier.data.ingredients);
     } catch (err) {
       console.log(err);
     }
   }, [router.isReady, setCart]);
+
+  useEffect(async () => {
+    if (!router.isReady || !cart) return;
+    try {
+      const ingredient = await axios.get(
+        "http://localhost:8000/api/ingredient/menu/" + cart.cart.menu_id,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer " + localStorage.getItem("sanctum_token"),
+          },
+        }
+      );
+      setIngredients(ingredient.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [router.isReady, cart]);
 
   return (
     <div className=" grid grid-cols-10 grid-rows-6 h-screen w-screen">
@@ -115,13 +146,68 @@ export default function cartItem() {
             )}
           </div>
           <div className="flex px-8 py-2 justify-around w-full">
-            <div className="text-[#795F53] font-bold">Quantity</div>
-            <div className="text-[#795F53]">
-              {cart ? cart.cart.quantity : ""}
+            <div className="text-[#795F53] font-bold flex">Ingredients</div>
+            <div className="flex flex-col gap-3">
+              {ingredients.map((elt) => {
+                return (
+                  <label key={elt.id} className="inline-flex items-center mt-3">
+                    <input
+                      type="checkbox"
+                      className="form-checkbox h-5 w-5 text-gray-600"
+                      defaultChecked={ings.includes(elt.name)}
+                      onChange={(e) => {
+                        if (e.target.checked) {
+                          setIngs([...ings, elt.name]);
+                        } else {
+                          let newIngs = ings;
+                          newIngs.splice(ings.indexOf(elt.name), 1);
+                          setIngs(newIngs);
+                        }
+                      }}
+                    />
+                    <span className="ml-2 text-gray-700">{elt.name}</span>
+                  </label>
+                );
+              })}
             </div>
           </div>
-          <div className="">
-            <button onClick={update}>update</button>
+          <div className="flex px-8 py-2 justify-around w-full">
+            <div className="text-[#795F53] font-bold">Quantity</div>
+            <div className="flex gap-3">
+              <FontAwesomeIcon
+                icon={faMinus}
+                className="text-[#ECE5F0] bg-[#795F53] p-1 cursor-pointer rounded-md hover:bg-[#ECE5F0] hover:text-[#795F53]"
+                onClick={() => {
+                  const updatedQuantity = {
+                    ...cart.cart,
+                    quantity: quantity - 1,
+                  };
+                  setQuantity(quantity - 1);
+                  setCart({ ...cart, cart: updatedQuantity });
+                }}
+              />
+              <div className="text-[#795F53]">{cart ? quantity : ""}</div>
+              <FontAwesomeIcon
+                icon={faPlus}
+                className="text-[#ECE5F0] bg-[#795F53] p-1 cursor-pointer rounded-md hover:bg-[#ECE5F0] hover:text-[#795F53]"
+                onClick={() => {
+                  const updatedQuantity = {
+                    ...cart.cart,
+                    quantity: quantity + 1,
+                  };
+                  setQuantity(quantity + 1);
+                  setCart({ ...cart, cart: updatedQuantity });
+                }}
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              className="px-4 py-2 text-[#795F53] font-bold rounded-md ring-1 ring-[#795F53] hover:text-[#ECE5F0] hover:bg-[#795F53] hover:shadow-md hover:shadow-[#34271D]"
+              onClick={update}
+            >
+              update
+            </button>
           </div>
         </div>
       </div>
